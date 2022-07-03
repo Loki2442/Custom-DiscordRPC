f53847672fa7e8ae6680a14aa8f759e35c87071b
Swastik Kulshreshtha <kul.swas321tik@gmail.com>
2022-07-04 00:44:14 +0530

    Basic Code

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Custom-DiscordRPC
- A customisable Discord RPC
+ A Customisable Discord Rich Presence
+
+ ## Setup
+ * Get the latest release from [Releases](https://github.com/Loki2442/Custom-DiscordRPC/releases) Section.
+ * Create a `config.json` file in the root directory of the downloaded executable.
+ * Edit the JSON file according to the example given in [`configexample.json`](./configexample.json)
+ * Run the executable.
+
+ <details>
+  <summary>Details for config.json</summary>
+  
+  * Create an application in [Discord Developer Portal](https://discord.com/developers/applications).
+  * Use the Application ID thus given as Client ID.
+  * Go to the Rich Presence Section of your Application & under the Rich Presence Assets, Add Images(s) you want to use in the Rich Presence.
+  * Then, go to the Visualizer and set the desired Fields. Use the Show Code Menu to get the variables for `config.json` file.
+</details>
+
+## Contribution
+Fork this repository and make your changes. Then, submit a Merge Request along with some Details.
+
+### License
+This Product is Licensed under the Apache License, Version 2.0
---
+++ richpresence.py
@@ -0,0 +1,164 @@
+#    Copyright 2022 Loki2442
+
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+
+#        http://www.apache.org/licenses/LICENSE-2.0
+
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+from msvcrt import getch
+import pypresence
+import time
+import json
+
+starttime = time.time()
+
+def configFileRead():
+    configFile = open("config.json", 'r')
+    config = json.loads(configFile.read())
+    return config
+
+def UpdateRPC(config):
+    try:
+        state = config["state"]
+    except KeyError:
+        state="Not Set"
+
+    try:
+        details = config["details"]
+    except KeyError:
+        details = "Not Set"
+
+    try:
+        start = config["start"]
+    except KeyError:
+        start = 1
+
+    try:
+        end = config["end"]
+    except KeyError:
+        end = 1
+
+    try:
+        large_image = config["large_image"]
+    except KeyError:
+        large_image = "Not Set"
+
+    try:
+        large_text = config["large_image_text"]
+    except KeyError:
+        large_text = "Not Set"
+
+    try:
+        small_image = config["small_image"]
+    except KeyError:
+        small_image = "Not Set"
+
+    try:
+        small_text = config["small_image_text"]
+    except KeyError:
+        small_text = "Not Set"
+
+    try:
+        buttons = config["buttons"]
+    except KeyError:
+        buttons = None
+
+    RPC.update(
+        state = state,
+        details = details,
+        start = start,
+        end = end,
+        large_image = large_image,
+        large_text = large_text,
+        small_image = small_image,
+        small_text = small_text,
+        buttons = buttons
+        )
+
+    print("Presence Updated!")
+
+try:
+    config = configFileRead()
+    try:
+        client_id = config["clientID"]
+    except KeyError:
+        print("Client ID not set!")
+
+    try:
+        state = config["state"]
+    except KeyError:
+        state="Not Set"
+
+    try:
+        details = config["details"]
+    except KeyError:
+        details = "Not Set"
+
+    try:
+        start = config["start"]
+    except KeyError:
+        start = 1
+
+    try:
+        end = config["end"]
+    except KeyError:
+        end = 1
+
+    try:
+        large_image = config["large_image"]
+    except KeyError:
+        large_image = "Not Set"
+
+    try:
+        large_text = config["large_image_text"]
+    except KeyError:
+        large_text = "Not Set"
+
+    try:
+        small_image = config["small_image"]
+    except KeyError:
+        small_image = "Not Set"
+
+    try:
+        small_text = config["small_image_text"]
+    except KeyError:
+        small_text = "Not Set"
+
+    try:
+        buttons = config["buttons"]
+    except KeyError:
+        buttons = None
+
+    RPC = pypresence.Presence(client_id)
+    RPC.connect()
+
+    RPC.update(
+        state = state,
+        details = details,
+        start = start,
+        end = end,
+        large_image = large_image,
+        large_text = large_text,
+        small_image = small_image,
+        small_text = small_text,
+        buttons = buttons
+        )  # Sets the presence
+
+    print("Rich Presence has been set!")
+        
+except Exception as e:
+    print(e)
+    junk = getch()
+
+while True:
+    time.sleep(15)
+    x = configFileRead()
+    if x != config:
+        UpdateRPC(x)
